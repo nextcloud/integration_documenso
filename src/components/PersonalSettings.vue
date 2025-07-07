5e0060bd1394ed2905d7ff9088b61c5a983be860
Jana Peper <jana.peper@nextcloud.com>
2025-07-07 10:44:32 +0200

    chore: fix eslint warning
    Signed-off-by: Jana Peper <jana.peper@nextcloud.com>

--- src/components/PersonalSettings.vue
+++ src/components/PersonalSettings.vue
@@ -17,7 +17,7 @@
 					v-model="state.url"
 					type="text"
 					:disabled="connected === true"
-					placeholder='https://app.documenso.com/'
+					placeholder="https://app.documenso.com/"
 					@input="onInput">
 			</div>
 			<div class="line">
