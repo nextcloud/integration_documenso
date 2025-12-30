850c4764e8626c37534d5809a48d40d76f36541f
Julien Veyssier <julien-nc@posteo.net>
2025-12-30 11:59:12 +0100

    chore: adjust to file actions breaking changes in NC 33
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/filesplugin.js
+++ src/filesplugin.js
@@ -29,10 +29,10 @@ if (!OCA.Documenso) {
 
 const requestSignatureAction = new FileAction({
 	id: 'documenso-sign',
-	displayName: (nodes) => {
+	displayName: ({ nodes }) => {
 		return t('integration_documenso', 'Request signature with Documenso')
 	},
-	enabled(nodes, view) {
+	enabled({ nodes, view }) {
 		return !OCA.Documenso.ignoreLists.includes(view.id)
 			&& nodes.length === 1
 			&& !nodes.some(({ permissions }) => (permissions & Permission.READ) === 0)
@@ -40,9 +40,9 @@ const requestSignatureAction = new FileAction({
 			&& !nodes.some(({ mime }) => mime !== 'application/pdf')
 	},
 	iconSvgInline: () => DocumensoIcon,
-	async exec(node) {
-		OCA.Documenso.DocumensoModalVue.$children[0].setFileId(node.fileid)
-		OCA.Documenso.DocumensoModalVue.$children[0].showModal()
+	async exec({ nodes }) {
+		OCA.Documenso.DocumensoModalVue.setFileId(nodes[0].fileid)
+		OCA.Documenso.DocumensoModalVue.showModal()
 		return null
 	},
 })
